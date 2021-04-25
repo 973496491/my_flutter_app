b93172e0a85512d7774713bd278bcdde6d655d87
Lolko <973496491@qq.com>
2021-04-25 10:03:55 +0800

    WebView实现

--- lib/widget/local_nav.dart
+++ lib/widget/local_nav.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_app/model/common_model.dart';
+import 'package:my_flutter_app/widget/webview.dart';
 
 class LocalNav extends StatelessWidget {
   final List<CommonModel> localNavList;
@@ -32,7 +33,16 @@ class LocalNav extends StatelessWidget {
 
   Widget _item(BuildContext context, CommonModel model) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => WebView(
+                      url: model.url,
+                      statusBarColor: model.statusBarColor,
+                      hideAppBar: model.hideAppBar,
+                    )));
+      },
       child: Column(
         children: <Widget>[
           Image.network(
